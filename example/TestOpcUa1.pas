a9ef2e2082b5608a2d1114aec5a662160e2e0537
Andry Smirnov <Andry.Smirnov@gmail.com>
2026-03-02 15:03:24 +0300

    Update test files

--- example/TestOpcUa1.pas
+++ example/TestOpcUa1.pas
@@ -19,7 +19,21 @@ uses
   StdCtrls,
   ComCtrls,
   ExtCtrls,
-  open62541;
+  //
+  open62541,
+  open62541_client_config,
+  open62541_client_highlevel,
+  open62541_common,
+  open62541_nodeids,
+  open62541_types,
+  open62541_types_h,
+  open62541_types_generated,
+  open62541_types_generated_handling,
+  open62541_server_config,
+  open62541_server_config_default,
+  open62541_server,
+  open62541_statuscodes
+  ;
 
 type
   {$IF NOT DECLARED(PtrInt)}
